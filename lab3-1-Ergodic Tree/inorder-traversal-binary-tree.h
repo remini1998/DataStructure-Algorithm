027ee32462ed4321640923502ead18eb5d8a071e
往日。 <mwr123607@126.com>
2017-12-08 03:11:43 +0800

    add 31

---
+++ lab3-1-Ergodic Tree/inorder-traversal-binary-tree.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "tree-node.h"
+#include <stack>
+
+//template<class T>
+//typedef typename TreeNode<T> arg2_type;
+//
+//template<class T>
+//typedef void (*fun_ptr)(TreeNode<T>);
+
+template <typename T>
+void Traversal(TreeNode<T>* head, void(*fun)(TreeNode<T>*)) {
+	if (!head) return;
+	std::stack<TreeNode<T>*> s;
+	TreeNode<T>* now = head;
+	while (now || !s.empty()) {
+		while (now) {
+			s.push(now);
+			now = now->leftChild;
+		}
+		now = s.top();
+		s.pop();
+		fun(now);
+		now = now->rightChild;
+	}
+}
+
+template <typename T>
+void TraversalWithRecursive(TreeNode<T>* head, void(*fun)(TreeNode<T>*)) {
+	if (head->leftChild) 
+		TraversalWithRecursive(head->leftChild, fun);
+	fun(head);
+	if (head->rightChild) 
+		TraversalWithRecursive(head->rightChild, fun);
+}
+
---
+++ lab3-1-Ergodic Tree/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "inorder-traversal-binary-tree.h"
+#include "tree-node.h"
+using namespace std;
+
+TreeNode<int>* BuildTree() {
+
+	cout << "Test Tree:" << endl;
+	cout << "    1" << endl;
+	cout << "   / \\ " << endl;
+	cout << "  2  3" << endl;
+	cout << " /  / \\ " << endl;
+	cout << "4  5  6" << endl;
+	cout << "    \\ " << endl;
+	cout << "    7" << endl;
+
+	TreeNode<int>* head = new TreeNode<int>(1);
+	head->leftChild = new TreeNode<int>(2);
+	head->rightChild = new TreeNode<int>(3);
+	head->leftChild->leftChild = new TreeNode<int>(4);
+	head->rightChild->leftChild = new TreeNode<int>(5);
+	head->rightChild->rightChild = new TreeNode<int>(6);
+	head->rightChild->leftChild->rightChild = new TreeNode<int>(7);
+
+	return head;
+
+}
+
+void output(TreeNode<int>* node) {
+	cout << node->data;
+}
+
+
+int main() {
+	TreeNode<int>* tree = BuildTree();
+	cout << "µÝ¹éÖÐÐò±éÀú£º";
+	TraversalWithRecursive<int>(tree, &output);
+	cout << endl;
+	cout << "·ÇµÝ¹éÖÐÐò±éÀú£º";
+	Traversal<int>(tree, &output);
+	cout << endl;
+}
