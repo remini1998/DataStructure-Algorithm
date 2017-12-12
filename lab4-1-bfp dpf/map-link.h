5dc95f714a4ebc59ba7b91e08cdf4058717a5054
彼端。 <mwr1998@qq.com>
2017-12-13 01:04:28 +0800

    update

---
+++ lab4-1-bfp dpf/map-link.h
@@ -0,0 +1,19 @@
+#pragma once
+template <typename T>
+class MapNode;
+template <typename T>
+class MapLink
+{
+public:
+	MapNode<T>* dest;
+	MapLink<T>* next;
+	float weight = 0;
+	MapLink(): MapLink(0) {
+
+	}
+	MapLink(float weight) {
+		this->weight = weight;
+	}
+private:
+
+};
--- lab4-1-dfs bfs/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "map-link.h"
-#include "map-node.h"
-
-int main() {
-
-	return 0;
-}
